04d1621e060353090c7177e6b2898b9467b166c8
cameron-rowe <cam.rowe91@gmail.com>
2014-02-03 13:31:54 -0800

    Removed conflicting classes from specification.js

--- static/assets/js/ncb/specification.js
+++ static/assets/js/ncb/specification.js
@@ -1,62 +1,4 @@
 
-function ModelParameters(name, type, map)
-{
-	this.name = name;
-	this.type = type;
-	this.map = map || {};
-}
-
-ModelParameters.prototype.toJSON = function() {
-	return {"name" : this.name, "type" : this.type, "map" : this.map};
-}
-
-function CellGroup(name, numberOfCells, modelParameters, geoGen)
-{
-	this.name = name;
-	this.count = numberOfCells;
-	this.parameters = modelParameters;
-	this.geoGen = geoGen || {};
-}
-
-CellGroup.prototype.toJSON = function() {
-	return {"name" : this.name, "count" : this.count, "parameters" : this.parameters.toJSON(), "geometry" : this.geoGen};
-}
-
-function CellAlias()
-{
-	this.cellGroups = [];
-	for(var i = 0; i < arguments.length; i++)
-		this.cellGroups[i] = arguments[i];
-}
-
-CellAlias.prototype.toJSON = function() {
-	var obj = {"CellGroups" : []};
-
-	for(var i = 0; i < this.cellGroups.length; i++)
-		obj.CellGroups[i] = this.cellGroups[i].toJSON();
-
-	return obj;
-}
-
-function ConnectionGroup(name, presynaptic, postsynaptic, probability, modelParameters)
-{
-	this.name = name;
-	this.presynaptic = presynaptic;
-	this.postsynaptic = postsynaptic;
-	this.probability = probability;
-	this.parameters = modelParameters;
-}
-
-ConnectionGroup.prototype.toJSON = function() {
-	return {
-		"name" : this.name, 
-		"presynaptic" : this.presynaptic.toJSON(),
-		"postsynaptic" : this.postsynaptic.toJSON(),
-		"probability" : this.probability,
-		"ModelParameters" : this.parameters.toJSON()
-	};
-}
-
 function sendJSON(jsonObj) {
     var request = $.ajax({
 		url: '/json',
@@ -82,10 +24,7 @@ function getJSON() {
 
 // testing area
 $().ready(function() {
-    var x = new CellGroup("CellGroup1", 2000, new ModelParameters("param1", "izhikevich"));
-    var alias = new CellAlias(x,x,x);
-    var conn = new ConnectionGroup("name1", alias, alias, 0.65, new ModelParameters("param2", "LIF"));
-    
+    conn = {"name" : "Cameron", "age" : 22};
     sendJSON(conn);
     var recData = getJSON();
     console.log(recData);
