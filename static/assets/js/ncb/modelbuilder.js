ad800d2210d82c0b0aab33ccfa61700e6982639e
Daniel Chavez <chavezmd007@gmail.com>
2014-02-02 16:37:30 -0800

    updated angularjs and added angularstrap

--- static/assets/js/ncb/modelbuilder.js
+++ static/assets/js/ncb/modelbuilder.js
@@ -24,7 +24,7 @@ var indexs = [];
 var breadDepth = 1;
 var globalCellGroup = [];
 
-angular.module('ncbApp', ['ui.bootstrap']);
+var ncbApp = angular.module('ncbApp', ['ui.bootstrap', 'mgcrea.ngStrap', 'mgcrea.ngStrap.tooltip']);
 
 
 //scope for models in the left menu
@@ -60,6 +60,28 @@ function myModelsList($scope) {
 		inc++;
 
 	};
+
+
+	// get details about the model to display correct information within the popover
+	
+
+	$scope.getModelDetails = function(model) {
+		return "{{'{{model.parameters.a}}'}}";
+
+
+	};
+
+	ncbApp.directive('popover', function(){
+		return {
+			restrict: 'A',
+			link:function(scope, element, attrib){
+				scope.modelName = attrib.model
+			},
+			template:"{{'{{modelName}}'}}"
+	
+		}
+	})
+	
 
 	// changes color of model depending on if from personal or database
 	$scope.styleModel = function(dbType) {
@@ -534,42 +556,4 @@ function getIndex(source, attr, value) {
 	}
 }
 
-var bootstrap = angular.module("bootstrap", []);
-
-
-bootstrap.directive('popOver', function ($compile) {
-        var itemsTemplate = "<ul class='unstyled'><li ng-repeat='model in list'>{{ '{{item}}' }}</li></ul>";
-        var getTemplate = function (contentType) {
-            var template = '';
-            switch (contentType) {
-                case 'list':
-                    template = itemsTemplate;
-                    break;
-            }
-            return template;
-        }
-        return {
-            restrict: "A",
-            transclude: true,
-            template: "<span ng-transclude></span>",
-            link: function (scope, element, attrs) {
-                var popOverContent;
-                if (scope.list) {
-                    var html = getTemplate("list");
-                    popOverContent = $compile(html)(scope);                    
-                }
-                var options = {
-                    content: popOverContent,
-                    placement: "right",
-                    html: true,
-                    title: scope.title
-                };
-                $(element).popover(options);
-            },
-            scope: {
-                list: '=',
-                title: '@'
-            }
-        };
-    });
 
