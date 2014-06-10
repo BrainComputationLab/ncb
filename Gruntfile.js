276a4b7d9924ef443a2294489c3b0e4caf7fd6a1
Nathan Jordan <natedagreat27274@gmail.com>
2014-06-09 23:24:01 -0700

    moved from include to bake, failed at getting htmlmin to work with angular templates

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,147 +1,217 @@
 module.exports = function(grunt) {
-    grunt.initConfig({
-        pkg: grunt.file.readJSON('package.json'),
-        clean: ['dist/', 'tmp/'],
-        bowercopy: {
-            almond: {
-                files: {
-                    'dist/assets/js/vendor/almond.js': 'almond/almond.js'
-                }
-            },
-            'angular-strap': {
-                files: {
-                    'dist/assets/js/vendor/angular-strap.min.js': 'angular-strap/dist/angular-strap.min.js',
-                    'dist/assets/js/vendor/angular-strap.tpl.min.js': 'angular-strap/dist/angular-strap.tpl.min.js',
-                    'dist/assets/js/vendor/angular-strap.min.js.map': 'angular-strap/dist/angular-strap.min.js.map',
-                    'dist/assets/js/vendor/angular-strap.tpl.min.js.map': 'angular-strap/dist/angular-strap.tpl.min.js.map'
-                }
-            },
-            'angular-ui-bootstrap-bower': {
-                files: {
-                    'dist/assets/js/vendor/ui-bootstrap.min.js': 'angular-ui-bootstrap-bower/ui-bootstrap.min.js'
-                }
-            },
-            angular: {
-                files: {
-                    'dist/assets/js/vendor/angular.min.js.map': 'angular/angular.min.js.map',
-                    'dist/assets/js/vendor/angular.min.js': 'angular/angular.js'
-                }
-            },
-            'angular-animate': {
-                files: {
-                    'dist/assets/js/vendor/angular-animate.min.js': 'angular-animate/angular-animate.min.js'
-                }
-            },
-            'angular-motion': {
-                files: {
-                    'dist/assets/css/angular-motion.min.css': 'angular-motion/dist/angular-motion.min.css'
-                }
-            },
-            restangular: {
-                files: {
-                    'dist/assets/js/vendor/restangular.min.js': 'restangular/dist/restangular.min.js'
-                }
-            },
-            bootstrap: {
-                files: {
-                    'dist/assets/css/bootstrap.min.css': 'bootstrap/dist/css/bootstrap.min.css',
-                    'dist/assets/js/vendor/bootstrap.min.js': 'bootstrap/dist/js/bootstrap.min.js',
-                    'dist/assets/fonts/': 'bootstrap/dist/fonts/*'
-                }
-            },
-            'bootstrap-additions': {
-                files: {
-                    'dist/assets/css/bootstrap-additions.min.css': 'bootstrap-additions/dist/bootstrap-additions.min.css'
-                }
-            },
-            jquery: {
-                files: {
-                    'dist/assets/js/vendor/jquery.min.js': 'jquery/dist/jquery.js',
-                    'dist/assets/js/vendor/jquery.min.map': 'jquery/dist/jquery.min.map'
-                }
-            },
-            underscore: {
-                files: {
-                    'dist/assets/js/vendor/underscore.js': 'underscore/underscore.js'
-                }
-            },
-            requirejs: {
-                files: {
-                    'dist/assets/js/vendor/require.js': 'requirejs/require.js'
-                }
-            },
-            jssha: {
-                files: {
-                    'dist/assets/js/vendor/sha256.js': 'jssha/src/sha256.js'
-                }
-            }
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    clean: ['dist/', 'tmp/'],
+    bowercopy: {
+      'angular-strap': {
+        files: {
+          'tmp/vendorjs/angular-strap.js': 'angular-strap/dist/angular-strap.js',
+          'tmp/vendorjs/angular-strap.tpl.js': 'angular-strap/dist/angular-strap.tpl.js',
+        }
+      },
+      'angular-ui-bootstrap-bower': {
+        files: {
+          'tmp/vendorjs/ui-bootstrap.js': 'angular-ui-bootstrap-bower/ui-bootstrap.js'
+        }
+      },
+      angular: {
+        files: {
+          'tmp/vendorjs/angular.js': 'angular/angular.js'
+        }
+      },
+      'angular-animate': {
+        files: {
+          'tmp/vendorjs/angular-animate.js': 'angular-animate/angular-animate.js'
+        }
+      },
+      'angular-motion': {
+        files: {
+          'tmp/css/angular-motion.css': 'angular-motion/dist/angular-motion.css'
+        }
+      },
+      restangular: {
+        files: {
+          'tmp/vendorjs/restangular.js': 'restangular/dist/restangular.js'
+        }
+      },
+      bootstrap: {
+        files: {
+          'tmp/vendorjs/bootstrap.js': 'bootstrap/dist/js/bootstrap.js',
+          'tmp/css/bootstrap.css': 'bootstrap/dist/css/bootstrap.css',
+          'dist/assets/fonts/': 'bootstrap/dist/fonts/*'
+        }
+      },
+      'bootstrap-additions': {
+        files: {
+          'tmp/css/bootstrap-additions.css': 'bootstrap-additions/dist/bootstrap-additions.css'
+        }
+      },
+      jquery: {
+        files: {
+          'tmp/vendorjs/jquery.js': 'jquery/dist/jquery.js',
+        }
+      },
+      underscore: {
+        files: {
+          'tmp/vendorjs/underscore.js': 'underscore/underscore.js'
+        }
+      },
+      requirejs: {
+        files: {
+          'tmp/vendorjs/require.js': 'requirejs/require.js'
+        }
+      },
+      jssha: {
+        files: {
+          'tmp/vendorjs/sha256.js': 'jssha/src/sha256.js'
+        }
+      }
+    },
+    copy: {
+      dist: {
+        files: [
+          {
+            expand: true,
+            src: ['images/**', 'icons/**'],
+            dest: 'dist/assets/'
+          }
+        ]
+      },
+      debug: {
+        files: [
+          {
+            expand: true,
+            src: ['images/**', 'icons/**'],
+            dest: 'dist/assets/'
+          },
+          {
+            expand: true,
+            flatten: true,
+            src: ['tmp/css/styles.css'],
+            dest: 'dist/assets/css'
+          },
+          {
+            expand: true,
+            flatten: true,
+            src: ['tmp/vendorjs/vendor.js'],
+            dest: 'dist/assets/js'
+          },
+          {
+            expand: true,
+            flatten: true,
+            src: ['tmp/ncbjs/ncb.js'],
+            dest: 'dist/assets/js'
+          },
+          {
+            expand: true,
+            flatten: true,
+            src: ['tmp/index.html'],
+            dest: 'dist/'
+          }
+        ]
+      }
+    },
+    bake: {
+      build: {
+        files: {
+          'tmp/index.html': 'html/index.html'
+        }
+      }
+    },
+    htmlmin: {
+      build: {
+        options: {
+          removeComments: true,
+          collapseWhitespace: true
         },
-        copy: {
-            main: {
-                files: [
-                    {
-                        expand: true,
-                        src: ['images/**', 'icons/**'],
-                        dest: 'dist/assets/'
-                    },
-                    {
-                        expand: true,
-                        flatten: true,
-                        src: ['js/*.js'],
-                        dest: 'dist/assets/js/ncb'
-                    }
-                ]
-            }
-        },
-        includes: {
-            files: {
-                src: ['html/index.html'],
-                dest: 'tmp',
-                flatten: true,
-                includePath: 'html'
-            }
-        },
-        htmlmin: {
-            dist: {
-                options: {
-                    removeComments: true,
-                    collapseWhitespace: true
-                },
-                files: {
-                    'dist/index.html': 'tmp/index.html'
-                }
-            }
-        },
-        jshint: {
-            all: ['Gruntfile.js', 'js/*.js']
-        },
-        less: {
-            production: {
-                options: {
-                    cleancss: true
-                },
-                files: {
-                    "dist/assets/css/ncb.min.css": "less/main.less"
-                }
-            }
-        },
-        mochaTest: {
-            test: {
-                src: ['js/tests/*.js']
-            }
-        },
-    });
+        files: {
+          'dist/index.html': 'tmp/index.html'
+        }
+      }
+    },
+    jshint: {
+      all: ['Gruntfile.js', 'js/**.js']
+    },
+    less: {
+      files: {
+        "tmp/css/ncb.css": "less/main.less"
+      }
+    },
+    mochaTest: {
+      test: {
+        src: ['js/tests/*.js']
+      }
+    },
+    concat: {
+      vendorjs: {
+        src: [
+          'tmp/vendorjs/jquery.js',
+          'tmp/vendorjs/underscore.js',
+          'tmp/vendorjs/bootstrap.js',
+          'tmp/vendorjs/angular.js',
+          'tmp/vendorjs/restangular.js',
+          'tmp/vendorjs/angular-animate.js',
+          'tmp/vendorjs/angular-strap.js',
+          'tmp/vendorjs/angular-strap.tpl.js',
+          'tmp/vendorjs/ui-bootstrap.js',
+          'tmp/vendorjs/sha256.js'
+        ],
+        dest: 'tmp/vendorjs/vendor.js'
+      },
+      css: {
+        src: ['tmp/css/*.css'],
+        dest: 'tmp/css/styles.css'
+      },
+      ncbjs: {
+        src: [
+          'js/app.js',
+          'js/builder.services.js',
+          'js/builder.controllers.js'
+        ],
+        dest: 'tmp/ncbjs/ncb.js'
+      },
+    },
+    uglify: {
+      vendorjs: {
+        files: {
+          'dist/assets/js/vendor.js': 'tmp/vendorjs/vendor.js'
+        }
+      },
+      ncbjs: {
+        files: {
+        'dist/assets/js/ncb.js': 'tmp/ncbjs/ncb.js'
+        }
+      }
+    },
+    cssmin: {
+      options: {
+        keepSpecialComments: 0
+      },
+      minify: {
+        flatten: true,
+        expand: true,
+        src: 'tmp/css/styles.css',
+        dest: 'dist/assets/css'
+      }
+    }
+  });
 
-    grunt.loadNpmTasks('grunt-includes');
-    grunt.loadNpmTasks('grunt-mocha-test');
-    grunt.loadNpmTasks('grunt-contrib-jshint');
-    grunt.loadNpmTasks('grunt-contrib-copy');
-    grunt.loadNpmTasks('grunt-contrib-clean');
-    grunt.loadNpmTasks('grunt-contrib-less');
-    grunt.loadNpmTasks('grunt-contrib-requirejs');
-    grunt.loadNpmTasks('grunt-contrib-htmlmin');
-    grunt.loadNpmTasks('grunt-bowercopy');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-contrib-less');
+  grunt.loadNpmTasks('grunt-contrib-htmlmin');
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-mocha-test');
+  grunt.loadNpmTasks('grunt-bowercopy');
+  grunt.loadNpmTasks('grunt-bake');
 
-    grunt.registerTask('default', ['jshint', 'mochaTest', 'clean', 'includes',
-                       'copy', 'bowercopy', 'less', 'htmlmin']);
+  grunt.registerTask('build:dist', ['jshint', 'mochaTest', 'clean', 'bake:build',
+       'bowercopy', 'less', 'bake', 'concat', 'cssmin', 'copy:dist',
+       'uglify', 'htmlmin:build']);
+
+  grunt.registerTask('build:debug', ['jshint', 'mochaTest', 'clean', 'bake:build',
+       'bowercopy', 'less', 'bake', 'concat', 'copy:debug']);
 };
