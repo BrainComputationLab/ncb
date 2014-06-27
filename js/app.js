ba4ab6b0e0015585feccd4c56c249c68e8577261
jakubberlinski <jakubberlinski3@gmail.com>
2014-06-27 14:22:56 -0700

    started messing with angular

--- js/app.js
+++ js/app.js
@@ -1,3 +1,48 @@
 // Module and dependency declarations
 angular.module('ncb.model', ['restangular']);
 angular.module('ncb.builder', ['ncb.model', 'mgcrea.ngStrap']);
+
+var app = angular.module('builder', ['mgcrea.ngStrap']);
+
+  app.controller('builder.controller', ['$scope', 'model', function($scope, model) {
+    $scope.myTemp = '<h1>Test2</h1>';
+    var neuron = '<h1>Test1</h1>';
+    var group = '<h1>Test2</h1>';
+
+    $scope.placeTemplate = function(type) {
+      if(type === "neuron") {
+          $scope.myTemp = neuron;
+      }
+    };
+
+    $scope.addIzh = function() {
+      $scope.root = model.addElement(0);
+    };
+
+  }]);
+
+  app.factory('model', function() {
+    var root = {
+      neurons: []
+    };
+
+    var addIzh = function(id) {root.neurons.push(id);}
+
+    return {
+      addElement: function(id){
+        if(id === 0) {
+          addIzh(id);
+          return root;
+        }
+      },
+      getElements: function() {
+        return root;
+      }
+    };
+  });
+
+  app.directive('myTemplate', function() {
+    return {
+      template: '{{myTemp}}'
+    };
+  });
